61c014644890f07076e20b706f418f1a3d5fc246
Stiveys <stevomwaura@gmail.com>
2025-03-10 11:44:11 +0300

    new

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setup(
     name="theater_work",
     version="0.1",
     packages=find_packages(),
+    install_requires=[
+        "sqlalchemy<2.0",  # Pin SQLAlchemy to version less than 2.0
+        "pytest",
+    ],
 )
